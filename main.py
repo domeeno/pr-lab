9dbf45b4494350e0ba74d0645ad8f345917edb85
dominic_flocea <dominic.flocea@nitroagility.com>
2020-09-22 12:56:20 +0300

    Added dummy requests

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import requests
+import json
+
+URL = 'http://127.0.0.1:5000/'
+
+
 def execute():
-    pass
+    print(requests.get(URL).text)
+    content = requests.get(URL + 'register')
+    data = json.loads(content.text)
+    token = data['access_token']
+    print(requests.get(URL + 'home', headers={'X-Access-Token': token}).text)
+    print(requests.get(URL + 'route/1', headers={'X-Access-Token': token}).text)
 
 
 if __name__ == "__main__":
